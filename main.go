2dcc3988241108f5efff8d84c6e78309aad36f99
LahcenHaouch <lahcen.haouch@protonmail.com>
2024-09-13 22:35:09 +0200

    Create a simple server that handles nothing X)

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type apiHandler struct{}
+
+func (apiHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("Hello Lahcen"))
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	var serv http.Server
+	serv.Addr = ":8080"
+	mux := http.NewServeMux()
+	mux.Handle("/", apiHandler{})
+
+	fmt.Println("Listening on port:8080")
+	if err := serv.ListenAndServe(); err != nil {
+		fmt.Println("Error 500: %w", err)
+	}
 }
