99d85079faac896503b39b67b9c48b9286e44fad
LahcenHaouch <lahcen.haouch@protonmail.com>
2024-09-15 00:51:10 +0200

    Add handlers for api endpoints

--- main.go
+++ main.go
@@ -5,15 +5,45 @@ import (
 	"net/http"
 )
 
+type apiConfig struct {
+	fileServerHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		cfg.fileServerHits++
+		fmt.Println("count: ", cfg.fileServerHits)
+		next.ServeHTTP(res, req)
+	})
+}
+func (cfg *apiConfig) countHandler(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(200)
+	res.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileServerHits)))
+}
+func (cfg *apiConfig) resetHandler(res http.ResponseWriter, req *http.Request) {
+	cfg.fileServerHits = 0
+	res.WriteHeader(200)
+	res.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileServerHits)))
+}
+
+func healthzHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(200)
+	res.Write([]byte("OK"))
+}
+
 func main() {
-	const port string = "8080"
+	api := apiConfig{fileServerHits: 0}
 	mux := http.NewServeMux()
 	serv := http.Server{
-		Addr:    ":" + port,
+		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", api.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /api/healthz", healthzHandler)
+	mux.HandleFunc("GET /api/metrics", api.countHandler)
+	mux.HandleFunc("/api/reset", api.resetHandler)
 
 	fmt.Println("Listening on port:8080")
 	if err := serv.ListenAndServe(); err != nil {
